e23637adf77e62ff05955c29714f4d6f4a1f9384
QIMING SHI <20092141@mail.wit.ie>
2021-11-02 10:09:10 +0000

    E2E tests - Home page - Filtering by the title

--- cypress/integration/home-page.spec.js
+++ cypress/integration/home-page.spec.js
@@ -1,7 +1,28 @@
-describe("Home Page", () => {
-    beforeEach(() => {
-      cy.visit("/");
-    });
+let movies;    // List of movies from TMDB
+
+// Utility functions
+const filterByTitle = (movieList, string) =>
+  movieList.filter((m) => m.title.toLowerCase().search(string) !== -1);
+
+const filterByGenre = (movieList, genreId) =>
+  movieList.filter((m) => m.genre_ids.includes(genreId));
+
+describe("Home Page ", () => {
+  before(() => {
+    // Get movies from TMDB and store in movies variable.
+    cy.request(
+      `https://api.themoviedb.org/3/discover/movie?api_key=${Cypress.env(
+        "TMDB_KEY"
+      )}&language=en-US&include_adult=false&include_video=false&page=1`
+    )
+      .its("body")    // Take the body of HTTP response from TMDB
+      .then((response) => {
+        movies = response.results
+      })
+  })
+  beforeEach(() => {
+    cy.visit("/")
+  });
   
     describe("Base test", () => {
       it("displays page header", () => {
@@ -9,4 +30,27 @@ describe("Home Page", () => {
         cy.get("h1").contains("Filter the movies");
       });
     });
-  })
+    describe("Filtering", () => {
+        describe("By movie title", () => {
+          it("should only display movies with entered text in the title", () => {
+            let searchString = "m";
+            let matchingMovies = filterByTitle(movies, searchString);
+            cy.get("#filled-search").clear().type(searchString); // Enter m in text box
+            cy.get(".MuiCardHeader-content").should(
+              "have.length",
+              matchingMovies.length
+            );
+            // Do a second test for certainty!
+            searchString = "o";
+            matchingMovies = filterByTitle(movies, searchString);
+            cy.get("#filled-search").clear().type(searchString); // Enter m in text box
+            cy.get(".MuiCardHeader-content").should(
+              "have.length",
+              matchingMovies.length
+            );
+          });
+         });
+      });
+    });
+  
+  
